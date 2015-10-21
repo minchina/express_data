a5fc7a51efa65c5ebbee82c6fe2c78ad665744cd
minchina <minchina91@gmail.com>
2015-10-21 14:06:18 +0800

    delete some codes

--- public/javascripts/reactjs/index.js
+++ public/javascripts/reactjs/index.js
@@ -1,40 +1,79 @@
-var names=['zhangsan','lisi','wangwu'];
-var arr = [
-    <h1>Hello world!</h1>,
-<h2>React is awesome</h2>
-];
-
 var HelloMessage = React.createClass(
     {
-        render: function(){
+        render: function () {
             return <div>
-            Hello, {this.props.name} {this.props.id}
-<button type="button" className="btn btn-primary">click</button>
-    </div>
-}
-}
+                Hello, {this.props.name} {this.props.id}
+                <button type="button" className="btn btn-primary">click</button>
+            </div>
+        }
+    }
 );
 
 var NotesList = React.createClass(
     {
-        render: function(){
+        render: function () {
             return (
                 <ol>
-                {this.props.children.map(function(node){
-                    return <li>{node}</li>
-                })}
-        </ol>
-)
-}
-}
+                    {this.props.children.map(function (node) {
+                        return <li>{node}</li>
+                    })}
+                </ol>
+            )
+        }
+    }
 );
+var MyComponent = React.createClass({
+    handleClick: function() {
+        React.findDOMNode(this.refs.myTextInput).focus();
+    },
+    render: function() {
+        return (
+            <div>
+                <input type="text" ref="myTextInput" />
+                <input type="button" value="Focus the text input" onClick={this.handleClick} />
+            </div>
+        );
+    }
+});
+
+var UserGist = React.createClass({
+    getInitialState:function(){
+        return {
+            username:"",
+            lastGistUrl:""
+        }
+    },
+    componentDidMount:function(){
+        $.get(this.props.source, function(data){
+            var lastGist = data[0];
+            console.log(data);
+            if(this.isMounted()){
+                this.setState({
+                    username: lastGist.owner.login,
+                    lastGistUrl: lastGist.html_url
+                })
+            }
+        }.bind(this));
+    },
+
+    componentWillMount:function(){
+        console.log(123);
+    },
+
+    render: function() {
+        return (
+            <div>
+                <HelloMessage name="hello" id="world"/>
+                {this.state.username}'s last gist is
+                <a href={this.state.lastGistUrl}>here</a>.
+            </div>
+        );
+    }
+});
 
 
 
 React.render(
-<NotesList>
-<span>hello</span>
-<span>world</span>
-</NotesList>,
+    <UserGist source="https://api.github.com/users/octocat/gists" />,
     document.getElementById('projectDialog')
 );
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    //res.render('index', {title: 'Express'});
-    res.sendfile("views/pages/index.html")
+    res.render('index', {title: 'Express'});
+    //res.sendfile("views/pages/index.html")
 });
 
 router.get('/react', function(req, res){
