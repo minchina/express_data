e5212f3a540f99581de63f8aaef246fddb05ce97
minchina <minchina91@gmail.com>
2016-07-13 16:40:02 +0800

    revert the back end data

--- routes/api.js
+++ routes/api.js
@@ -5,4 +5,17 @@ router.get('/d3',function(req,res){
     res.sendfile("views/pages/d3.html");
 });
 
+router.post('/comparison', function(req,res){
+    res.json([
+        { year: '2008', value1: 20, value2:10},
+        { year: '2009', value1: 10 ,value2:11},
+        { year: '2010', value1: 5 ,value2:12},
+        { year: '2011', value1: 5 ,value2:13},
+        { year: '2012', value1: 20 ,value2:14 },
+        { year: '2013', value1: 25 ,value2:14 },
+        { year: '2014', value1: 20 ,value2:13 },
+        { year: '2015', value1: 20 ,value2:14 }
+    ]);
+});
+
 module.exports = router;
